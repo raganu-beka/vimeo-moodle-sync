b035232b0d89e6d511755605e95c14489ab2f0a4
raganu-beka <rebekapilscikova@gmail.com>
2026-03-08 22:20:37 +0200

    refactor: add argparse for dynamic date input, replace hardcoded date logic in session matching

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pprint import pprint
+import argparse
 
 import config
 from integrations.vimeo_client import VimeoClient
@@ -9,16 +10,23 @@ from parsing.recording_normalizer import normalize_recording
 from scheduling.schedule_day import get_sessions_for_date
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--day',
+        nargs='?',
+        type=lambda x: datetime.strptime(x, '%Y-%m-%d').date(),
+        default=datetime.now().date(),
+        help='Date to get match recordings for in YYYY-MM-DD format. Defaults to current day.'
+    )
+    args = parser.parse_args()
 
     settings = config.Settings()
-
     vimeo = VimeoClient(settings.vimeo_access_token)
 
-    day = datetime.now().date()
-    videos = vimeo.get_user_folder_videos_by_date(settings.vimeo_user_id, settings.vimeo_folder_id, day)
+    videos = vimeo.get_user_folder_videos_by_date(settings.vimeo_user_id, settings.vimeo_folder_id, args.day)
 
     courses = [parse_course_name(course, settings) for course in settings.courses]
-    sessions = get_sessions_for_date(courses, day, settings.timezone_name)
+    sessions = get_sessions_for_date(courses, args.day, settings.timezone_name)
     recordings = [normalize_recording(video, settings) for video in videos]
 
     pprint(match_session_recordings(sessions, recordings, settings))
